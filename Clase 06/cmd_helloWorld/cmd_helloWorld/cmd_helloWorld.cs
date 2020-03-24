8a5be578fd890eaa1f7584bffa3cd8bdf35ff568
Renzo Rios Rugel <rrios@flesan.com.pe>
2020-03-24 17:49:43 -0500

    PreClase07

---
+++ Clase 06/cmd_helloWorld/cmd_helloWorld/cmd_helloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//NameSpaces Revit
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace cmd_helloWorld
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class cmd_helloWorld : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, 
+                              ref string message, 
+                              ElementSet elements)
+        {
+            TaskDialog.Show("Mi primer mensaje", "Este es mi primer hola mundo con la API de Revit");
+            return Result.Succeeded;
+        }
+    }
+}
